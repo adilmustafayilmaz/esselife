01d272ff3071b148ce73d08ac0c14ed18b76b660
adilmustafayilmaz <79438419+adilmustafayilmaz@users.noreply.github.com>
2024-04-27 15:02:11 +0300

    Addin motion to the applications page

--- my-app/app/(user)/applications/page.tsx
+++ my-app/app/(user)/applications/page.tsx
@@ -1,67 +1,87 @@
 'use client'
 import TextComponent from '@/components/TextComponent'
 import React from 'react'
-import Typewriter from 'typewriter-effect';
+
+import {motion} from "framer-motion";
 
 
 export default function Applications() {
-  return (
-    <>
-      <div className='flex flex-col'>
+    return (
+        <>
+
+            <div className='flex flex-col'>
+
+                <motion.div
+                    className="box"
+                    initial={{opacity: 0, scale: 0.5}}
+                    animate={{opacity: 1, scale: 1}}
+                    transition={{
+                        duration: 0.8,
+                        delay: 0.2,
+                        ease: [0, 0.71, 0.2, 1.01]
+                    }}>
+                    <div className='lg:flex max-lg:flex-col lg:justify-around'>
+                        <TextComponent header='Işık' imageName='/isik.jpeg' imageStyle={{objectFit: "cover"}}
+                                       style="bg-[#F2FFE9]">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut perferendis rerum
+                                laboriosam provident corporis dolorum architecto consectetur rem officiis consequuntur
+                            </p>
+                        </TextComponent>
+                    </div>
+                </motion.div>
+
+                <div className='lg:flex max-lg:flex-col lg:justify-around'>
+
+                    <motion.div
+                        className="box"
+                        initial={{opacity: 0, scale: 0.5}}
+                        animate={{opacity: 1, scale: 1}}
+                        transition={{
+                            duration: 0.8,
+                            delay: 0.2,
+                            ease: [0, 0.71, 0.2, 1.01]
+                        }}>
+                        <TextComponent header='Hypervolt' imageName='/hypervolt1.jpeg' style='bg-[#F2FFE9]'
+                                       imageStyle={{objectFit: "cover"}}>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut perferendis rerum
+                                laboriosam provident corporis dolorum architecto consectetur rem officiis consequuntur f
+                                uga ad laborum voluptates eveniet, porro officia magni explicabo impedit nostrum. Sint
+                                fa
+
+                            </p>
+
+                        </TextComponent>
+
+                    </motion.div>
 
 
-        <div className='lg:flex max-lg:flex-col lg:justify-around'>
-          <TextComponent header='Işık' imageName='/isik.jpeg' imageStyle={{ objectFit: "cover" }} style="bg-[#F2FFE9]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut perferendis rerum
-              laboriosam provident corporis dolorum architecto consectetur rem officiis consequuntur f
-              uga ad laborum voluptates eveniet, porro officia magni explicabo impedit nostrum. Sint fa
-              cere quam exercitationem amet similique nulla harum nam. Ea magni similique expedita in mo
-              lestiae esse. Dolores repudiandae ullam eveniet tempora fugiat labore est placeat accusanti
-              um iste! Officia amet nobis repudiandae suscipit accusantium consequatur facilis ad beatae,
-              ratione eius soluta sint delectus totam similique, ducimus voluptas quas. Explicabo archi
-              tecto voluptatibus ullam animi qui delectus enim, iure quo nemo fuga. Cumque in beatae nisi
-              doloribus maxime, molestiae nam. Voluptate.
-            </p>
+                    <motion.div
+                        className="box"
+                        initial={{opacity: 0, scale: 0.5}}
+                        animate={{opacity: 1, scale: 1}}
+                        transition={{
+                            duration: 0.8,
+                            delay: 0.2,
+                            ease: [0, 0.71, 0.2, 1.01]
+                        }}>
+                        <TextComponent header='Elektrik' imageName='/elektrik.jpeg' imageStyle={{objectFit: "cover"}}
+                                       style="bg-[#F2FFE9]">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut perferendis rerum
+                                laboriosam provident corporis dolorum architecto consectetur rem officiis consequuntur f
+                                uga ad laborum voluptates eveniet, porro officia magni explicabo impedit nostrum. Sint
+                                fa
+                            </p>
 
-          </TextComponent>
-        </div>
-        
-        <div className='lg:flex max-lg:flex-col lg:justify-around'>
-          <TextComponent header='Hypervolt' imageName='/hypervolt1.jpeg' style='bg-[#F2FFE9]' imageStyle={{ objectFit: "cover" }}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut perferendis rerum
-              laboriosam provident corporis dolorum architecto consectetur rem officiis consequuntur f
-              uga ad laborum voluptates eveniet, porro officia magni explicabo impedit nostrum. Sint fa
-              cere quam exercitationem amet similique nulla harum nam. Ea magni similique expedita in mo
-              lestiae esse. Dolores repudiandae ullam eveniet tempora fugiat labore est placeat accusanti
-              um iste! Officia amet nobis repudiandae suscipit accusantium consequatur facilis ad beatae,
-              ratione eius soluta sint delectus totam similique, ducimus voluptas quas. Explicabo archi
-              tecto voluptatibus ullam animi qui delectus enim, iure quo nemo fuga. Cumque in beatae nisi
-              doloribus maxime, molestiae nam. Voluptate.
-            </p>
+                        </TextComponent>
+                    </motion.div>
 
-          </TextComponent>
+                </div>
 
 
-          <TextComponent header='Elektrik' imageName='/elektrik.jpeg' imageStyle={{ objectFit: "cover" }} style="bg-[#F2FFE9]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut perferendis rerum
-              laboriosam provident corporis dolorum architecto consectetur rem officiis consequuntur f
-              uga ad laborum voluptates eveniet, porro officia magni explicabo impedit nostrum. Sint fa
-              cere quam exercitationem amet similique nulla harum nam. Ea magni similique expedita in mo
-              lestiae esse. Dolores repudiandae ullam eveniet tempora fugiat labore est placeat accusanti
-              um iste! Officia amet nobis repudiandae suscipit accusantium consequatur facilis ad beatae,
-              ratione eius soluta sint delectus totam similique, ducimus voluptas quas. Explicabo archi
-              tecto voluptatibus ullam animi qui delectus enim, iure quo nemo fuga. Cumque in beatae nisi
-              doloribus maxime, molestiae nam. Voluptate.
-            </p>
-
-          </TextComponent>
-        </div>
-
-
-      </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
--- my-app/app/(user)/appointment/page.tsx
+++ my-app/app/(user)/appointment/page.tsx
@@ -1,13 +1,24 @@
 'use client'
+import { motion } from 'framer-motion';
 import React from 'react'
-import Typewriter from 'typewriter-effect';
+
 
 
 export default function Appointment() {
   return (
-    <>
-      <div className='flex justify-center'>Appointment</div>
+      <>
 
-    </>
+          <motion.div
+              className="box"
+              initial={{opacity: 0, scale: 0.5}}
+              animate={{opacity: 1, scale: 1}}
+              transition={{
+                  duration: 0.8,
+                  delay: 0.2,
+                  ease: [0, 0.71, 0.2, 1.01]
+              }}>
+
+          </motion.div>
+      </>
   )
 }
